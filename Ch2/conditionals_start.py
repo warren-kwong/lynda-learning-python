cdb4117fc54776f48c839f1b63501663d8c528dc
Warren <warren.p.kwong@gmail.com>
2018-09-25 21:20:45 -0700

    conditional statements if, elif, else, "a if C else b"

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -2,13 +2,22 @@
 # Example file for working with conditional statements
 #
 
-def main():
-  x, y = 10, 100
-  
-  # conditional flow uses if, elif, else  
 
-  # conditional statements let you use "a if C else b"
-  
+def main():
+    x, y = 100, 100
+
+    # conditional flow uses if, elif, else
+    if (x < y):
+        st = "x is less than y"
+    elif (x == y):
+        st = "x is the same as y"
+    else:
+        st = "x is greater than y"
+    # conditional statements let you use "a if C else b"
+    st = "x is less than y" if (
+        x < y) else "x is greater than or the same as y"
+    print(st)
+
 
 if __name__ == "__main__":
-  main()
+    main()
