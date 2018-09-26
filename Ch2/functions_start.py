502a6e9233149a477cbb8a3c7f8e35493324a7ac
Warren <warren.p.kwong@gmail.com>
2018-09-25 21:15:48 -0700

    finished functions

--- Ch2/functions_start.py
+++ Ch2/functions_start.py
@@ -5,14 +5,46 @@
 # define a basic function
 
 
+def func1():
+    print('this is a function')
+
 # function that takes arguments
 
 
-# function that returns a value
+def func2(arg1, arg2):
+    print(arg1, " ", arg2)
 
+
+# function that returns a value
+def cube(x):
+    return x*x*x
 
 # function with default value for an argument
 
 
-#function with variable number of arguments
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result = result * num
+    return result
 
+# function with variable number of arguments
+
+
+def multi_add(*args):
+    result = 0
+    for x in args:
+        result = result + x
+    return result
+
+
+# func1()
+# print(func1())
+# print(func1)
+# func2(10, 20)
+# print(func2(10, 20))
+# print(cube(3))
+# print(power(2))
+# print(power(2, 3))
+# print(power(x=3, num=2))
+print(multi_add(4, 5, 10, 4))
